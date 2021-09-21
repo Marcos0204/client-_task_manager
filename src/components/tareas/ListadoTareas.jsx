a6f1ade64a249b7e5df35d897d2da24b8eac2b04
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-21 16:11:13 -0400

    listado-Tareas

---
+++ src/components/tareas/ListadoTareas.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+    const tareas = [
+        {nombre:'elegir plataforma', estado:true},
+        {nombre:'elegir colores', estado:false },
+        {nombre:'elegir plataformas de pago', estado:false },
+        {nombre:'elegir Hosting', estado: true}
+    ]
+    return (
+        <>
+            <h2>Proyecto: Tienda virtual</h2>
+            <ul className="listado-tareas">
+                { tareas.length === 0
+                    ? (<li className='tarea'><p>No Hay Tareas</p> </li>)
+                    : tareas.map((tarea, index) =>(
+                        <Tarea tarea={tarea} key={index}/>
+                    ))
+                }
+            </ul>
+        </>
+    )
+}
+
+export default ListadoTareas
