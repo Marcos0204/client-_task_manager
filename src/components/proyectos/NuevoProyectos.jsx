86ab8205b51b2025e98baa679513db8f0f24e6e0
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-21 14:19:35 -0400

    state-funtion--nuevoProyecto

--- src/components/proyectos/NuevoProyectos.jsx
+++ src/components/proyectos/NuevoProyectos.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const NuevoProyectos = () => {
+
+    const [ proyectos, guardarProyectos ] = useState({
+        nombre:''
+    })
+    const {nombre} = proyectos;
+
+    const onChangeProyecto = (e) =>{
+
+        guardarProyectos({
+            ...proyectos,
+            [e.target.name]: e.target.value
+        })
+    }
     return (
         <>
             <button
@@ -17,6 +30,8 @@ const NuevoProyectos = () => {
                     className='input-text'
                     placeholder='Nombre del Proyectos'
                     name='nombre'
+                    value={nombre}
+                    onChange={onChangeProyecto}
                 />
                 <input
                     type="submit"
