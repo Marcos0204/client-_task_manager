130dd6ba7751c15d5e87e77b2896f5e9618a936f
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-09-23 01:28:33 -0400

    funtion-get-task

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,15 +1,24 @@
 import React, {useContext} from 'react';
 import ProjectContext from '../../context/proyectos/ProjectContext';
+import TaskContext from '../../context/task/taskContext';
 
 const Project = ({project}) => {
     const proyectosContext = useContext(ProjectContext);
+    const taskContext = useContext(TaskContext);
+
     const { actualProject } = proyectosContext;
+    const {getTask} = taskContext;
     
+    const selectProject = id =>{
+        actualProject(id);
+        getTask(id)
+    }
+
     return (
         <li>
             <button
                 className='btn btn-blank'
-                onClick={()=> actualProject(project.id)}
+                onClick={()=> selectProject(project.id)}
             >
                 {project.name}
             </button>
--- src/context/task/taskState.js
+++ src/context/task/taskState.js
@@ -3,25 +3,48 @@ import React, {useReducer} from "react";
 import TaskContext from "./taskContext";
 import TaskReducer from "./taskReducer";
 
+import { TASKS_PROJECT
+        
+        } from "../../types";
+
 
 const TaskState = props => {
 
     const initialSate = {
         task:[
-            {name:'elegir plataforma', state:true},
-            {name:'elegir colores', state:false },
-            {name:'elegir plataformas de pago', state:false },
-            {name:'elegir Hosting', state: true}
+        {name:'elegir plataforma', state:true, projectId: 1},
+        {name:'elegir colores', state:false, projectId:2},
+        {name:'elegir plataformas de pago', state:false, projectId:3 },
+        {name:'elegir Hosting', state: true, projectId:4},
+        {name:'elegir plataforma', state:true, projectId: 1},
+        {name:'elegir colores', state:false, projectId:2},
+        {name:'elegir plataformas de pago', state:false, projectId:3 },
+        {name:'elegir Hosting', state: true, projectId:4},
+        {name:'elegir plataforma', state:true, projectId: 1},
+        {name:'elegir colores', state:false, projectId:2},
+        {name:'elegir plataformas de pago', state:false, projectId:3 },
+        {name:'elegir Hosting', state: true, projectId:4},
         ]
     }
 
     // state and dispatch
     const [state, dispatch] = useReducer(TaskReducer, initialSate);
 
+
+    ///funtions
+
+    //get tasks projects
+    const getTask = projectID =>
+    dispatch({
+        type: TASKS_PROJECT,
+        payload: projectID
+    })
+
     return(
         <TaskContext.Provider
             value={{
-                task:state.task
+                task:state.task,
+                getTask
             }}
         >
             {props.children}
